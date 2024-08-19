2f6363478ba2ad3c8536989f3bb26a70c301439c
PetrKavan31 <petrkavan31@gmail.com>
2024-08-19 17:59:08 +0200

    edit

--- myproj/todo/views.py
+++ myproj/todo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+
 # Create your views here.
